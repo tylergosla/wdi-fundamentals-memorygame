2b66c7cd296cf228123cdd10d43bf718a8e861e0
tylergosla <tyler.gosla@gmail.com>
2018-08-08 21:13:26 -0400

    create logic for memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,25 @@
-console.log("Up and running!");
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "Queen";
+var cards = ["queen", "queen", "king", "king"];
+//create emoty array named cardsInPlay
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+//move a card to cardsInPlay array
+cardsInPlay.push('queen');
+// show in the console that a card was flipped
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//move another card to cardsInPlay array
+var cardTwo = cards[2]
+
+cardsInPlay.push('king');
+// show in the console that a card was flipped
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('you found a match');
+	} else {
+		alert('sorry, try again')
+	}
+
+}
