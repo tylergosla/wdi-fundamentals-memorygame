45ca810e0f419a9e793ce110b5a688a545138052
tylergosla <tyler.gosla@gmail.com>
2018-08-10 19:33:23 -0400

    updated memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
-console.log("Up and running!");
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "Queen";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+//create emoty array named cardsInPlay
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert('you found a match');
+    } else {
+        alert('sorry, try again')
+    };
+
+}
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId])
+    console.log("User flipped" + " " + cards[cardId]);
+    if (cardsInPlay.length === 2) {
+		checkForMatch();
+    }
+};
+
+flipCard(0);
+flipCard(2);
