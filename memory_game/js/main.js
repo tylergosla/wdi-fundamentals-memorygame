aa59bd2d38b013c324c688586ae16afdd5699f22
tylergosla <tyler.gosla@gmail.com>
2018-08-11 19:57:35 -0400

    uodated memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,30 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    }
+];
+
+
 //create emoty array named cardsInPlay
 var cardsInPlay = [];
 
@@ -16,8 +39,10 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId])
-    console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank)
+    console.log("User flipped" + " " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     if (cardsInPlay.length === 2) {
 		checkForMatch();
     }
